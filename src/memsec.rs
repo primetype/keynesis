6e2c4ad3f394212afd11a716b6d29f3fb647a70e
Nicolas Di Prima <nicolas@primetype.co.uk>
2021-01-22 09:06:49 +0000

    update the memsec module with unit tests and some panic checks

--- src/memsec.rs
+++ src/memsec.rs
@@ -32,6 +32,10 @@ pub unsafe fn memset(dst: *mut u8, val: u8, count: usize) {
 
 /// compare the equality of the 2 given arrays, constant in time
 ///
+/// # Panics
+///
+/// The function will panic if it is called with a `len` of 0.
+///
 /// # Safety
 ///
 /// Expecting to have both valid pointer and the count to fit in
@@ -39,6 +43,11 @@ pub unsafe fn memset(dst: *mut u8, val: u8, count: usize) {
 #[inline(never)]
 pub unsafe fn memeq(v1: *const u8, v2: *const u8, len: usize) -> bool {
     let mut sum = 0;
+
+    assert!(
+        len != 0,
+        "Cannot perform equality comparison if the length is 0"
+    );
 
     for i in 0..len {
         let val1 = ptr::read_volatile(v1.add(i));
@@ -54,6 +63,10 @@ pub unsafe fn memeq(v1: *const u8, v2: *const u8, len: usize) -> bool {
 
 /// Constant time comparison
 ///
+/// # Panics
+///
+/// The function will panic if it is called with a `len` of 0.
+///
 /// # Safety
 ///
 /// Expecting to have both valid pointer and the count to fit in
@@ -61,6 +74,12 @@ pub unsafe fn memeq(v1: *const u8, v2: *const u8, len: usize) -> bool {
 #[inline(never)]
 pub unsafe fn memcmp(v1: *const u8, v2: *const u8, len: usize) -> std::cmp::Ordering {
     let mut res = 0;
+
+    assert!(
+        len != 0,
+        "Cannot perform ordering comparison if the length is 0"
+    );
+
     for i in (0..len).rev() {
         let val1 = ptr::read_volatile(v1.add(i)) as i32;
         let val2 = ptr::read_volatile(v2.add(i)) as i32;
@@ -149,5 +168,75 @@ impl<T: Scrubbed> Scrubbed for std::cell::Cell<T> {
 impl<T: Scrubbed> Scrubbed for std::cell::RefCell<T> {
     fn scrub(&mut self) {
         self.get_mut().scrub()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::cmp::Ordering;
+
+    use super::*;
+    use quickcheck::TestResult;
+
+    #[test]
+    #[should_panic]
+    fn eq_empty() {
+        let bytes = Vec::new();
+        unsafe { memeq(bytes.as_ptr(), bytes.as_ptr(), bytes.len()) };
+    }
+
+    #[test]
+    #[should_panic]
+    fn ord_empty() {
+        let bytes = Vec::new();
+        unsafe { memcmp(bytes.as_ptr(), bytes.as_ptr(), bytes.len()) };
+    }
+
+    #[quickcheck]
+    fn eq(bytes: Vec<u8>) -> TestResult {
+        if bytes.is_empty() {
+            TestResult::discard()
+        } else {
+            let b = unsafe { memeq(bytes.as_ptr(), bytes.as_ptr(), bytes.len()) };
+            TestResult::from_bool(b)
+        }
+    }
+
+    #[quickcheck]
+    fn ord_eq(bytes: Vec<u8>) -> TestResult {
+        if bytes.is_empty() {
+            TestResult::discard()
+        } else {
+            let ord = unsafe { memcmp(bytes.as_ptr(), bytes.as_ptr(), bytes.len()) };
+            TestResult::from_bool(ord == Ordering::Equal)
+        }
+    }
+
+    #[quickcheck]
+    fn neq(a: Vec<u8>, b: Vec<u8>) -> TestResult {
+        let len = std::cmp::min(a.len(), b.len());
+
+        if a == b || len == 0 {
+            TestResult::discard()
+        } else {
+            let b = unsafe { memeq(a.as_ptr(), b.as_ptr(), len) };
+
+            TestResult::from_bool(!b)
+        }
+    }
+
+    #[quickcheck]
+    fn ord(a: Vec<u8>, b: Vec<u8>) -> TestResult {
+        let len = std::cmp::min(a.len(), b.len());
+
+        if len == 0 {
+            TestResult::discard()
+        } else {
+            let a = &a[..len];
+            let b = &b[..len];
+            let ord = unsafe { memcmp(a.as_ptr(), b.as_ptr(), len) };
+
+            TestResult::from_bool(ord == a.cmp(b))
+        }
     }
 }
