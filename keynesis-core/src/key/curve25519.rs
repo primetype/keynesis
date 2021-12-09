d093d760a32b0dffc0092f182243ec78c04e9279
Nicolas Di Prima <nicolas@primetype.co.uk>
2021-12-09 16:58:18 +0000

    allow user's to access the inner see of the secret key

--- keynesis-core/src/key/curve25519.rs
+++ keynesis-core/src/key/curve25519.rs
@@ -86,7 +86,7 @@ impl SecretKey {
     ///
     /// be mindful that leaking the content of the internal signing key
     /// may result in losing the ultimate control of the signing key
-    pub(crate) fn leak_as_ref(&self) -> &[u8; Self::SIZE] {
+    pub fn leak_as_ref(&self) -> &[u8; Self::SIZE] {
         &self.secret
     }
 }
